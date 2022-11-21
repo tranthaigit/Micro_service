cdc27904746d6907449c2f2fe02074107d3db904
tranhoangthaicv <83809894+tranhoangthaicv@users.noreply.github.com>
2022-11-22 00:42:20 +0700

    finish grpc logger

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -24,8 +24,8 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
-
 	defer rabbitConn.Close()
+
 	app := Config{
 		Rabbit: rabbitConn,
 	}
@@ -50,11 +50,11 @@ func connect() (*amqp.Connection, error) {
 	var backOff = 1 * time.Second
 	var connection *amqp.Connection
 
-	// don't continue untill Rabbitmq is ready
+	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
-			fmt.Println("Rabbitmq not ready yet")
+			fmt.Println("RabbitMQ not yet ready...")
 			counts++
 		} else {
 			log.Println("Connected to RabbitMQ!")
@@ -68,7 +68,7 @@ func connect() (*amqp.Connection, error) {
 		}
 
 		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
-		log.Println("backing off")
+		log.Println("backing off...")
 		time.Sleep(backOff)
 		continue
 	}
